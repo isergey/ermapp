ebdbde5ab4e73e8be7dd87a88c71970238693ed4
Sergey <isergey@inbox.ru>
2011-06-09 19:12:56 +0400

    init db

--- settings.py
+++ settings.py
@@ -44,6 +44,7 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
+
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
 USE_L10N = True
