22f3dcd8cb10a921f5aa59911aa8653e644c8429
Sergey <isergey@inbox.ru>
2011-06-17 18:56:03 +0400

    add RPNQueryBuilder

--- apps/erm/views.py
+++ apps/erm/views.py
@@ -10,12 +10,12 @@ import appsettings
 
 journals_base = appsettings.ZBASES['journals']
 zconnection = pyaz.ZConnection(
-    {
+        {
         'user': journals_base['server']['user'],
-        'password':journals_base['server']['password'],
+        'password': journals_base['server']['password'],
         'databaseName': journals_base['server']['databaseName'],
         'preferredRecordSyntax': journals_base['server']['preferredRecordSyntax'],
-    }
+        }
 )
 
 zconnection.connect(str(journals_base['server']['host']), int(journals_base['server']['port']))
@@ -29,29 +29,38 @@ def index(request):
 
 
 
-
 def search_resources(request):
     search_results = []
-    if 'term' in request.GET:
-        zresults = zconnection.search(u'@attr 1=4 %s' % 'Philosophy')
+
+    if 'term' in request.GET and request.GET['term']:
+        qb = pyaz.RPNQueryBuilder()
+        if 'ft'not in request.GET:
+            qb.add_condition(term=request.GET['term'], use=u'4', structure=u"6")
+            query = qb.build()
+        else:
+            qb.add_condition(term=request.GET['term'], use=u'4')
+            query = qb.build()
+        
+        #print query, type(query)
+        zresults = zconnection.search(query)
         search_results = []
         offset = 0
         number = offset # порядковый номер записи в результате
-        print zresults.get_size()
+
         for zrecord in zresults.get_records(offset, 20):
             number += 1
             search_results.append(
-                {
+                    {
                     'number': number,
-                    'record': pymarc.Record(data=zrecord,to_unicode=True, encoding='utf-8')
+                    'record': pymarc.Record(data=zrecord, to_unicode=True, encoding='utf-8')
                 }
             )
-    
+
     return render_to_response(
         'index.html',
-        {
+            {
             'message': _('Hello'),
             'search_results': search_results,
-        },
+            },
         context_instance=RequestContext(request)
     )
